9943d2cde7b50fbf91087c1d269ea343c0dc12ce
ReverseCoder1 <meetkhanpara095@gmail.com>
2026-04-08 23:38:31 +0530

    add toml file 123

--- server/app.py
+++ server/app.py
@@ -7,3 +7,7 @@ import uvicorn
 def main() -> None:
     """CLI entrypoint for `server` script in pyproject.toml."""
     uvicorn.run("server.app:app", host="0.0.0.0", port=7860)
+
+
+if __name__ == "__main__":
+    main()
